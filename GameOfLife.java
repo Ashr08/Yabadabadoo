52e61d6be551bbe42d057e1f8ff75fe4fd49db26
Ashr08 <ashrithgandluru@gmail.com>
2025-03-11 05:19:07 +0000

    finished step 1

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,81 +1,71 @@
-import java.util.Arrays;
+public class GameOfLife {
+    private final int width, height;
+    private int[][] board, nextBoard;
 
-public class GameOfLife implements Board {
-
-    // Integers: 0 or 1 for alive or dead
-    private int[][] board;
-
-    public GameOfLife(int x, int y)
-    {
-        // Construct a 2d array of the given x and y size.
+    public GameOfLife(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.board = new int[height][width];
+        this.nextBoard = new int[height][width];
     }
 
-    // Set values on the board
-    public void set(int x, int y, int[][] data) {
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                board[i + x][j + y] = data[i][j];
-            }
+    public void run(int steps) {
+        for (int i = 0; i < steps; i++) {
+            step();
         }
     }
 
-    // Run the simulation for a number of turns
-    public void run(int turns) {
-        // call step the number of times requested
+    public void step() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int neighbors = countNeighbors(x, y);
+                if (board[y][x] == 1) {
+                    nextBoard[y][x] = (neighbors == 2 || neighbors == 3) ? 1 : 0;
+                } else {
+                    nextBoard[y][x] = (neighbors == 3) ? 1 : 0;
+                }
+            }
+        }
+        int[][] temp = board;
+        board = nextBoard;
+        nextBoard = temp;
     }
-
-    // Step the simulation forward one turn.
-    public void step()
-    {
-        print();
-        // Update the game board, store a 1 if the cell is alive and a 0 otherwise.
-    }
-
 
     public int countNeighbors(int x, int y) {
         int count = 0;
-        // count the number of neighbors the cell has
-        // use the get(x,y) method to read any board state you need.
+        for (int dy = -1; dy <= 1; dy++) {
+            for (int dx = -1; dx <= 1; dx++) {
+                if (dx == 0 && dy == 0) continue;
+                if (get(x + dx, y + dy) == 1) count++;
+            }
+        }
         return count;
     }
 
-    // Get a value from the board with "wrap around"
-    // Locations outside the board will loop back into the board.
-    // Ex: -1 will read board.length-1
     public int get(int x, int y) {
-        int xLimit = board.length;
-        int yLimit= board[0].length;
-        return board[(x+xLimit)%xLimit][(y+yLimit)%yLimit];
+        int nx = (x + width) % width;
+        int ny = (y + height) % height;
+        return board[ny][nx];
     }
 
-    // Test helper to get the whole board state
-    public int[][] get()
-    {
-        return board;
-    }
-
-    // Test helper to print the current state
-    public void print(){
-        // Print the header
-        System.out.print("\n ");
-        for (int y = 0; y < board[0].length; y++) {
-            System.out.print(y%10 + " ");
-        }
-
-        for (int x = 0; x < board.length; x++) {
-            System.out.print("\n" + x%10);
-            for (int y=0; y<board[x].length; y++)
-            {
-                if (board[x][y] == 1)
-                {
-                    System.out.print("⬛");
-                }
-                else
-                {
-                    System.out.print("⬜");
-                }
+    public void set(int x, int y, int[][] data) {
+        for (int dy = 0; dy < data.length; dy++) {
+            for (int dx = 0; dx < data[dy].length; dx++) {
+                int nx = (x + dx) % width;
+                int ny = (y + dy) % height;
+                board[ny][nx] = data[dy][dx];
             }
+        }
+    }
+
+    public void print() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(board[y][x] == 1 ? "█ " : ". ");
+            }
+            System.out.println();
         }
         System.out.println();
     }
 }
+
